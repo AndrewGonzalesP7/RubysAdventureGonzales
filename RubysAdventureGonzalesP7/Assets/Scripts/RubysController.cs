45f9a4764e385eca1ea6a208c3e8250714aafe7d
AndrewGonzalesP7 <730630@student.dusd.net>
2022-11-29 15:23:31 -0800

    Health
    added health Collectible and added rubys health script but still not finished

--- RubysAdventureGonzalesP7/Assets/Scripts/RubysController.cs
+++ RubysAdventureGonzalesP7/Assets/Scripts/RubysController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RubysController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -13,6 +16,8 @@ public class RubysController : MonoBehaviour
         //QualitySettings.VSyncCount = 0;
         //Application.targetFrameRate = 10;
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -34,5 +39,11 @@ public class RubysController : MonoBehaviour
         position.x = position.x + 6.0f * horizontal * Time.deltaTime;
         position.y = position.y + 6.0f * vertical * Time.deltaTime;
         rigidbody2d.MovePosition(position);
+
+    }
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
